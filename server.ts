212d971567af11d3bf5586b49daf67be9b508cf1
Benjamin Pryor <bscott23@gmail.com>
2021-10-11 14:31:12 -0400

    first commit

---
+++ server.ts
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const socket = require("socket.io");
+
+const router = require("./router");
+const passport = require("passport");
+
+mongoose.connect("mongodb://localhost/aaronnds");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+router(app);
+
+const server = app.listen(5000, () => {
+  console.log("Node.js listening on port " + 5000);
+});
+
+const io = socket(server);
+app.set("io", io);
+
+io.on("connection", (socket) => {
+  console.log("Socket connected: ", socket.id);
+});
